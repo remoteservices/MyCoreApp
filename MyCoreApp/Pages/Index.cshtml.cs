5215690aa903ec4a4fafb39725f3e1c67a13f4d1
Hershel McClain <remoteservices19@gmail.com>
2022-12-13 22:58:16 -0500

    MyCoreApp Files

---
+++ MyCoreApp/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCoreApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+       
+    }
+}
+
+
+
+
+/*
+ 
+ 7. Next, create a string for the current date that is formatted for 
+ * your culture or region by using the DateTime.ToString method.
+ 
+ */
